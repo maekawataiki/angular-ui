58a9556891db8fda42e45397fa06dc99421f0942
maekawataiki <maekawataiki@yahoo.co.jp>
2019-07-12 07:02:07 -0400

    navbar elevation on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,17 @@ import { ThemeService } from './core/theme/theme.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  offset: number = 0;
 
   constructor(public themeService: ThemeService) {
   }
 
   ngOnInit() {
     this.themeService.initTheme();
+  }
+
+  onScroll(event) {
+    this.offset = event.target.scrollTop;
   }
 
 }
